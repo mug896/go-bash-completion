5cbf2354f8d5df4f60881760e439c4f04f1d6a6e
mug896 <mug896@gmail.com>
2022-06-03 11:46:53 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# go-bash-completion
+## Go Bash Completion
+
+Copy contents of go-bash-completion.sh to ~/.bash_completion  
+open new terminal and try completion
+
+
+```sh
+bash$ go version 
+go version go1.18.2 linux/amd64
+
+bash$ go [tab]
+bug       doc       fmt       help      mod       tool      work      
+build     env       generate  install   run       version   
+clean     fix       get       list      test      vet 
+```
---
+++ go-bash-completion.sh
@@ -0,0 +1,47 @@
+_go() 
+{
+    local CMD=${COMP_WORDS[0]}
+    local CUR=${COMP_WORDS[COMP_CWORD]}
+    local PREV=${COMP_WORDS[COMP_CWORD-1]}
+    local IFS=$' \t\n' WORDS
+    local HCMD=$( echo "${COMP_LINE% *}" | sed -En 's/\s+-.*$//; s/^\s*'$CMD'\s+(help)?/'$CMD' help /; p' );
+    local HCMD2=$( echo "${COMP_LINE% *}" | sed -En 's/'$CMD'\s+tool\s+-n\s+/'$CMD' tool /; s/\s+-.*$//; s/$/ --help/; p' );
+    local SED_CMD='sed -En '\''/[Cc]ommands are:$/{ n; :X n; s/^[[:blank:]]*([^[:blank:]]+).*/\1/p; tX }'\'
+    local SED_OPT=$( cat <<\@
+            sed -En -e '1bZ' -e 's/^[[:blank:]]+(-[[:alnum:]-]+).*/\1/; TB; p; b' \
+                    -e ':B /^The -/!b; :X s/ flag/&/; TY; bZ; :Y N; bX' \
+                    -e ':Z s/(.*)([^[:alnum:]]-[[:alnum:]-]+)(.*)/\2\n\1/; /-[[:alnum:]-]+\n$/{ s/[^[:alnum:]\n-]//gp; b}; tZ'
+@
+)
+    if [ "${CUR:0:1}" = "-" ]; then
+        if [[ ${COMP_WORDS[1]} = "tool" && $COMP_CWORD -ge 3 ]]; then
+            WORDS=$( $HCMD2 |& eval "$SED_OPT" )
+        else
+            WORDS=$( $HCMD |& eval "$SED_OPT" )
+            case ${COMP_WORDS[1]} in
+                clean | get | install | list | run | test | vet)
+                WORDS=$WORDS$'\n'$( $CMD help build |& eval "$SED_OPT" ) ;;
+            esac
+        fi
+    else
+        if [ "$COMP_CWORD" -eq 1 ]; then
+            WORDS=$( $CMD help |& eval "$SED_CMD" )$' help'
+        else
+            if [ "${COMP_WORDS[1]}" = "tool" ]; then
+                if [[ $COMP_CWORD -eq 2 || ( $COMP_CWORD -eq 3 && $PREV = "-n" ) ]]; then
+                    WORDS=$( $CMD tool )
+                else
+                    WORDS=$( $HCMD2 |& eval "$SED_CMD" )
+                fi
+            else
+                WORDS=$( $HCMD |& eval "$SED_CMD" )
+            fi
+        fi
+    fi
+    COMPREPLY=( $(compgen -W "$WORDS" -- "$CUR") )
+    [ "${COMPREPLY: -1}" = "=" ] && compopt -o nospace
+}
+
+complete -o default -o bashdefault -F _go go
+
+
